1ae580435bde1c92998952a92b9d5e3fc5041118
Grigorii Soloviev <gsoloviev100@yandex-team.ru>
2019-08-07 14:55:25 +0400

    comment unused code

--- Common/Classes/PauseTimer.swift
+++ Common/Classes/PauseTimer.swift
@@ -1,22 +1,23 @@
 import UIKit
 
-class PauseTimer {
-
-	private let timer: Timer? = nil
-	private var pauseStartDate: Date? = nil
-	private var previousFireDate: Date? = nil
-	
-	private func pauseTimer() {
-		guard let timer = self.timer else { return }
-		pauseStartDate = Date()
-		previousFireDate = timer.fireDate
-		timer.fireDate = Date.distantFuture
-	}
-	
-	private func resumeTimer() {
-		guard let pauseStartDate = pauseStartDate, let previousFireDate = previousFireDate else { return }
-		let pauseTime = pauseStartDate.timeIntervalSinceNow * -1
-		self.timer?.fireDate = Date(timeInterval: pauseTime, since: previousFireDate)
-	}
-	
-}
+//TODO: создать PauseTimer и выннести из PauseTimer FullScreenPresenter-а
+//class PauseTimer {
+//
+//	private let timer: Timer? = nil
+//	private var pauseStartDate: Date? = nil
+//	private var previousFireDate: Date? = nil
+//
+//	private func pauseTimer() {
+//		guard let timer = self.timer else { return }
+//		pauseStartDate = Date()
+//		previousFireDate = timer.fireDate
+//		timer.fireDate = Date.distantFuture
+//	}
+//
+//	private func resumeTimer() {
+//		guard let pauseStartDate = pauseStartDate, let previousFireDate = previousFireDate else { return }
+//		let pauseTime = pauseStartDate.timeIntervalSinceNow * -1
+//		self.timer?.fireDate = Date(timeInterval: pauseTime, since: previousFireDate)
+//	}
+//
+//}
