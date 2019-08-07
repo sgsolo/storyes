423ef89abfca44ad42d1c4a4e718ddd48c39d425
Grigorii Soloviev <gsoloviev100@yandex-team.ru>
2019-08-07 17:18:43 +0400

    fix code style
    https://st.yandex-team.ru/MSTORIES-9

--- Modules/FullScreen/Views/ProgressView.swift
+++ Modules/FullScreen/Views/ProgressView.swift
@@ -12,20 +12,19 @@ struct ProgressModel {
 }
 
 class ProgressView: UIView {
-	var collectionViewAdapter: ProgressCollectionViewAdapterInput = ProgressCollectionViewAdapter()
-	
-	private var collectionView: UICollectionView = {
-		let layout = UICollectionViewFlowLayout()
-		layout.minimumInteritemSpacing = 0
-		layout.minimumLineSpacing = 0
-		let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-		return collectionView
-	}()
 	static let leftRightInset: CGFloat = 16
 	static let topBottomInset: CGFloat = 0
 	static let cellSpacing: CGFloat = 8
+
+	var collectionViewAdapter: ProgressCollectionViewAdapterInput = ProgressCollectionViewAdapter()
+	
+	private var collectionView: UICollectionView
 	
 	override init(frame: CGRect) {
+		let layout = UICollectionViewFlowLayout()
+		layout.minimumInteritemSpacing = 0
+		layout.minimumLineSpacing = 0
+		collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
 		super.init(frame: frame)
 		configureCollectionView()
 	}
@@ -34,7 +33,7 @@ class ProgressView: UIView {
 		fatalError("init(coder:) has not been implemented")
 	}
 	
-	func configureCollectionView() {
+	private func configureCollectionView() {
 		self.addSubview(collectionView)
 		collectionView.backgroundColor = .clear
 		collectionView.translatesAutoresizingMaskIntoConstraints = false
